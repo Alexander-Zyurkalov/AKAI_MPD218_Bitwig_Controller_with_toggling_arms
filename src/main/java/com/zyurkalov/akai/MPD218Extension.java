b4e206b521dcd1292eece88d88cf0749545b60d6
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2023-04-29 18:36:29 +0200

    code made by ChatGPT

--- src/main/java/com/zyurkalov/akai/MPD218Extension.java
+++ src/main/java/com/zyurkalov/akai/MPD218Extension.java
@@ -1,67 +1,45 @@
 package com.zyurkalov.akai;
 
-import com.bitwig.extension.api.util.midi.ShortMidiMessage;
-import com.bitwig.extension.callback.ShortMidiMessageReceivedCallback;
-import com.bitwig.extension.controller.api.ControllerHost;
-import com.bitwig.extension.controller.api.Transport;
 import com.bitwig.extension.controller.ControllerExtension;
+import com.bitwig.extension.controller.api.ControllerHost;
+import com.bitwig.extension.controller.api.CursorDevice;
+import com.bitwig.extension.controller.api.MidiIn;
+import com.bitwig.extension.controller.api.NoteInput;
 
-public class MPD218Extension extends ControllerExtension
-{
-   protected MPD218Extension(final MPD218ExtensionDefinition definition, final ControllerHost host)
-   {
+public class MPD218Extension extends ControllerExtension {
+
+   private MidiIn midiInPort;
+   private NoteInput padNotes;
+   private CursorDevice cursorDevice;
+
+   protected MPD218Extension(MPD218ExtensionDefinition definition, ControllerHost host) {
       super(definition, host);
    }
 
    @Override
-   public void init()
-   {
-      final ControllerHost host = getHost();      
+   public void init() {
+      midiInPort = getHost().getMidiInPort(0);
+      padNotes = midiInPort.createNoteInput("MPD218 Pads", "89????", "99????", "DF????", "EF??", "AF????");
+      midiInPort.setMidiCallback(this::onMidi);
+      cursorDevice = getHost().createEditorCursorDevice();
+      getHost().println("Akai Professional MPD218 Bitwig Controller Script");
+   }
 
-      mTransport = host.createTransport();
-      host.getMidiInPort(0).setMidiCallback((ShortMidiMessageReceivedCallback)msg -> onMidi0(msg));
-      host.getMidiInPort(0).setSysexCallback((String data) -> onSysex0(data));
+   private void onMidi(int status, int data1, int data2) {
+      if (status == 0xb1) {
+         cursorDevice.getMacro(data1).getAmount().inc(uint7ToInt7(data2), 128);
+      }
+   }
 
-      // TODO: Perform your driver initialization here.
-      // For now just show a popup notification for verification that it is running.
-      host.showPopupNotification("MPD218 Initialized");
+   private int uint7ToInt7(int value) {
+      return value - 64;
    }
 
    @Override
-   public void exit()
-   {
-      // TODO: Perform any cleanup once the driver exits
-      // For now just show a popup notification for verification that it is no longer running.
-      getHost().showPopupNotification("MPD218 Exited");
+   public void exit() {
    }
 
    @Override
-   public void flush()
-   {
-      // TODO Send any updates you need here.
+   public void flush() {
    }
-
-   /** Called when we receive short MIDI message on port 0. */
-   private void onMidi0(ShortMidiMessage msg) 
-   {
-      // TODO: Implement your MIDI input handling code here.
-   }
-
-   /** Called when we receive sysex MIDI message on port 0. */
-   private void onSysex0(final String data) 
-   {
-      // MMC Transport Controls:
-      if (data.equals("f07f7f0605f7"))
-            mTransport.rewind();
-      else if (data.equals("f07f7f0604f7"))
-            mTransport.fastForward();
-      else if (data.equals("f07f7f0601f7"))
-            mTransport.stop();
-      else if (data.equals("f07f7f0602f7"))
-            mTransport.play();
-      else if (data.equals("f07f7f0606f7"))
-            mTransport.record();
-   }
-
-   private Transport mTransport;
 }
