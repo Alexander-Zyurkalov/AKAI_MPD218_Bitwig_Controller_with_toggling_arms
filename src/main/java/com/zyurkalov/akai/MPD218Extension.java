bea2e5bf09055ad21a370e80bf8f6c1b42545f8b
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2023-04-29 19:00:34 +0200

    changes to slow

--- src/main/java/com/zyurkalov/akai/MPD218Extension.java
+++ src/main/java/com/zyurkalov/akai/MPD218Extension.java
@@ -1,16 +1,14 @@
 package com.zyurkalov.akai;
 
 import com.bitwig.extension.controller.ControllerExtension;
-import com.bitwig.extension.controller.api.ControllerHost;
-import com.bitwig.extension.controller.api.CursorDevice;
-import com.bitwig.extension.controller.api.MidiIn;
-import com.bitwig.extension.controller.api.NoteInput;
+import com.bitwig.extension.controller.api.*;
 
 public class MPD218Extension extends ControllerExtension {
 
    private MidiIn midiInPort;
    private NoteInput padNotes;
    private CursorDevice cursorDevice;
+   private CursorRemoteControlsPage remoteControlsPage;
 
    protected MPD218Extension(MPD218ExtensionDefinition definition, ControllerHost host) {
       super(definition, host);
@@ -21,13 +19,18 @@ public class MPD218Extension extends ControllerExtension {
       midiInPort = getHost().getMidiInPort(0);
       padNotes = midiInPort.createNoteInput("MPD218 Pads", "89????", "99????", "DF????", "EF??", "AF????");
       midiInPort.setMidiCallback(this::onMidi);
-      cursorDevice = getHost().createEditorCursorDevice();
+      cursorDevice = getHost().createCursorTrack("MPD218_CursorTrack", "MPD218 Cursor Track", 0, 0, true).createCursorDevice("MPD218_CursorDevice", "MPD218 Cursor Device", 0, CursorDeviceFollowMode.FIRST_DEVICE);
+      remoteControlsPage = cursorDevice.createCursorRemoteControlsPage( 8);
+
       getHost().println("Akai Professional MPD218 Bitwig Controller Script");
+      getHost().showPopupNotification("MPD218 Initialized");
    }
 
    private void onMidi(int status, int data1, int data2) {
       if (status == 0xb1) {
-         cursorDevice.getMacro(data1).getAmount().inc(uint7ToInt7(data2), 128);
+         double delta = uint7ToInt7(data2) / 128.0;
+         remoteControlsPage.getParameter(data1).inc(delta, 128);
+
       }
    }
 
@@ -37,6 +40,7 @@ public class MPD218Extension extends ControllerExtension {
 
    @Override
    public void exit() {
+        getHost().showPopupNotification("MPD218 Exited");
    }
 
    @Override
