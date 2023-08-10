b40ec4b3cd7c9f5fb1e54bdff6f33a6255afa8de
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2023-08-10 10:49:55 +0200

    reformatting the code

--- src/main/java/com/zyurkalov/akai/MPD218Extension.java
+++ src/main/java/com/zyurkalov/akai/MPD218Extension.java
@@ -5,73 +5,72 @@ import com.bitwig.extension.controller.api.*;
 
 public class MPD218Extension extends ControllerExtension {
 
-   private MidiIn midiInPort;
-   private MidiOut midiOutPort;
-   private NoteInput noteInput;
-   private CursorDevice cursorDevice;
-   TrackBank trackBank;
-   private CursorRemoteControlsPage remoteControlsPage;
+    private MidiIn midiInPort;
+    private MidiOut midiOutPort;
+    private NoteInput noteInput;
+    private CursorDevice cursorDevice;
+    TrackBank trackBank;
+    private CursorRemoteControlsPage remoteControlsPage;
 
-   protected MPD218Extension(MPD218ExtensionDefinition definition, ControllerHost host) {
-      super(definition, host);
-   }
+    protected MPD218Extension(MPD218ExtensionDefinition definition, ControllerHost host) {
+        super(definition, host);
+    }
 
-   @Override
-   public void init() {
-      midiInPort = getHost().getMidiInPort(0);
-      noteInput = midiInPort.createNoteInput("MPD218 Pads", "89????", "99????", "DF????", "EF??", "AF????", "D?????");
-      midiInPort.setMidiCallback(this::onMidi);
+    @Override
+    public void init() {
+        midiInPort = getHost().getMidiInPort(0);
+        noteInput = midiInPort.createNoteInput("MPD218 Pads", "89????", "99????", "DF????", "EF??", "AF????", "D?????");
+        midiInPort.setMidiCallback(this::onMidi);
 
-      midiOutPort = getHost().getMidiOutPort(0);
-      cursorDevice = getHost().createCursorTrack("MPD218_CursorTrack", "MPD218 Cursor Track", 0, 0, true).createCursorDevice("MPD218_CursorDevice", "MPD218 Cursor Device", 0, CursorDeviceFollowMode.FIRST_DEVICE);
-      remoteControlsPage = cursorDevice.createCursorRemoteControlsPage( 6);
-      for (int i = 0; i < 6; i++) {
+        midiOutPort = getHost().getMidiOutPort(0);
+        cursorDevice = getHost().createCursorTrack("MPD218_CursorTrack", "MPD218 Cursor Track", 0, 0, true).createCursorDevice("MPD218_CursorDevice", "MPD218 Cursor Device", 0, CursorDeviceFollowMode.FIRST_DEVICE);
+        remoteControlsPage = cursorDevice.createCursorRemoteControlsPage(6);
+        for (int i = 0; i < 6; i++) {
             remoteControlsPage.getParameter(i).markInterested();
             remoteControlsPage.getParameter(i).setIndication(true);
-      }
+        }
 
-      trackBank = getHost().createTrackBank(4*4, 0, 0);
-      for (int i = 0; i < trackBank.getSizeOfBank(); i++) {
-         trackBank.getItemAt(i).arm().markInterested();
-         trackBank.getItemAt(i).mute().markInterested();
-         int finalI = i;
-         trackBank.getItemAt(i).arm().addValueObserver( (value) -> {
-            midiOutPort.sendMidi(0x90, 36 + finalI, value ? 127 : 0);
-         });
-         trackBank.getItemAt(i).mute().addValueObserver( (value) -> {
-            midiOutPort.sendMidi(0x90, 52 + finalI, value ? 127 : 0);
-         });
-      }
-      getHost().println("Akai Professional MPD218 Bitwig Controller Script");
-      getHost().showPopupNotification("MPD218 Initialized");
-   }
+        trackBank = getHost().createTrackBank(4 * 4, 0, 0);
+        for (int i = 0; i < trackBank.getSizeOfBank(); i++) {
+            trackBank.getItemAt(i).arm().markInterested();
+            trackBank.getItemAt(i).mute().markInterested();
+            int finalI = i;
+            trackBank.getItemAt(i).arm().addValueObserver((value) -> {
+                midiOutPort.sendMidi(0x90, 36 + finalI, value ? 127 : 0);
+            });
+            trackBank.getItemAt(i).mute().addValueObserver((value) -> {
+                midiOutPort.sendMidi(0x90, 52 + finalI, value ? 127 : 0);
+            });
+        }
+        getHost().println("Akai Professional MPD218 Bitwig Controller Script");
+        getHost().showPopupNotification("MPD218 Initialized");
+    }
 
-   private void onMidi(int status, int data1, int data2) {
-      if (status == 0xb1) {
-         remoteControlsPage.getParameter(data1).inc(uint7ToInt7(data2), 128);
-      }
-      else if (status == 0x80) {
-         int armNum = data1 - 36;
-         if (armNum >=0 && armNum < trackBank.getSizeOfBank()) {
-            trackBank.getItemAt(armNum).arm().toggle();
-         }
-         int muteNum = data1 - 52;
-         if (muteNum >=0 && muteNum < trackBank.getSizeOfBank()) {
-             trackBank.getItemAt(muteNum).mute().toggle();
-         }
-      }
-   }
+    private void onMidi(int status, int data1, int data2) {
+        if (status == 0xb1) {
+            remoteControlsPage.getParameter(data1).inc(uint7ToInt7(data2), 128);
+        } else if (status == 0x80) {
+            int armNum = data1 - 36;
+            if (armNum >= 0 && armNum < trackBank.getSizeOfBank()) {
+                trackBank.getItemAt(armNum).arm().toggle();
+            }
+            int muteNum = data1 - 52;
+            if (muteNum >= 0 && muteNum < trackBank.getSizeOfBank()) {
+                trackBank.getItemAt(muteNum).mute().toggle();
+            }
+        }
+    }
 
-   private int uint7ToInt7(int value) {
-      return value >= 64 ? value - 128 : value;
-   }
+    private int uint7ToInt7(int value) {
+        return value >= 64 ? value - 128 : value;
+    }
 
-   @Override
-   public void exit() {
+    @Override
+    public void exit() {
         getHost().showPopupNotification("MPD218 Exited");
-   }
+    }
 
-   @Override
-   public void flush() {
-   }
+    @Override
+    public void flush() {
+    }
 }
--- src/main/java/com/zyurkalov/akai/MPD218ExtensionDefinition.java
+++ src/main/java/com/zyurkalov/akai/MPD218ExtensionDefinition.java
@@ -1,4 +1,5 @@
 package com.zyurkalov.akai;
+
 import java.util.UUID;
 
 import com.bitwig.extension.api.PlatformType;
@@ -6,94 +7,76 @@ import com.bitwig.extension.controller.AutoDetectionMidiPortNamesList;
 import com.bitwig.extension.controller.ControllerExtensionDefinition;
 import com.bitwig.extension.controller.api.ControllerHost;
 
-public class MPD218ExtensionDefinition extends ControllerExtensionDefinition
-{
-   private static final UUID DRIVER_ID = UUID.fromString("528486ae-3668-4147-af48-0aae60968563");
-   
-   public MPD218ExtensionDefinition()
-   {
-   }
+public class MPD218ExtensionDefinition extends ControllerExtensionDefinition {
+    private static final UUID DRIVER_ID = UUID.fromString("528486ae-3668-4147-af48-0aae60968563");
 
-   @Override
-   public String getName()
-   {
-      return "MPD218";
-   }
-   
-   @Override
-   public String getAuthor()
-   {
-      return "Alexandr Zyurkalov";
-   }
+    public MPD218ExtensionDefinition() {
+    }
 
-   @Override
-   public String getVersion()
-   {
-      return "0.1";
-   }
+    @Override
+    public String getName() {
+        return "MPD218";
+    }
 
-   @Override
-   public UUID getId()
-   {
-      return DRIVER_ID;
-   }
-   
-   @Override
-   public String getHardwareVendor()
-   {
-      return "Akai";
-   }
-   
-   @Override
-   public String getHardwareModel()
-   {
-      return "MPD218";
-   }
+    @Override
+    public String getAuthor() {
+        return "Alexandr Zyurkalov";
+    }
 
-   @Override
-   public int getRequiredAPIVersion()
-   {
-      return 17;
-   }
+    @Override
+    public String getVersion() {
+        return "0.1";
+    }
 
-   @Override
-   public int getNumMidiInPorts()
-   {
-      return 1;
-   }
+    @Override
+    public UUID getId() {
+        return DRIVER_ID;
+    }
 
-   @Override
-   public int getNumMidiOutPorts()
-   {
-      return 1;
-   }
+    @Override
+    public String getHardwareVendor() {
+        return "Akai";
+    }
 
-   @Override
-   public void listAutoDetectionMidiPortNames(final AutoDetectionMidiPortNamesList list, final PlatformType platformType)
-   {
-      if (platformType == PlatformType.WINDOWS)
-      {
-         // TODO: Set the correct names of the ports for auto detection on Windows platform here
-         // and uncomment this when port names are correct.
-         // list.add(new String[]{"Input Port 0"}, new String[]{"Output Port 0"});
-      }
-      else if (platformType == PlatformType.MAC)
-      {
-         // TODO: Set the correct names of the ports for auto detection on Windows platform here
-         // and uncomment this when port names are correct.
-         // list.add(new String[]{"Input Port 0"}, new String[]{"Output Port 0"});
-      }
-      else if (platformType == PlatformType.LINUX)
-      {
-         // TODO: Set the correct names of the ports for auto detection on Windows platform here
-         // and uncomment this when port names are correct.
-         // list.add(new String[]{"Input Port 0"}, new String[]{"Output Port 0"});
-      }
-   }
+    @Override
+    public String getHardwareModel() {
+        return "MPD218";
+    }
 
-   @Override
-   public MPD218Extension createInstance(final ControllerHost host)
-   {
-      return new MPD218Extension(this, host);
-   }
+    @Override
+    public int getRequiredAPIVersion() {
+        return 17;
+    }
+
+    @Override
+    public int getNumMidiInPorts() {
+        return 1;
+    }
+
+    @Override
+    public int getNumMidiOutPorts() {
+        return 1;
+    }
+
+    @Override
+    public void listAutoDetectionMidiPortNames(final AutoDetectionMidiPortNamesList list, final PlatformType platformType) {
+        if (platformType == PlatformType.WINDOWS) {
+            // TODO: Set the correct names of the ports for auto detection on Windows platform here
+            // and uncomment this when port names are correct.
+            // list.add(new String[]{"Input Port 0"}, new String[]{"Output Port 0"});
+        } else if (platformType == PlatformType.MAC) {
+            // TODO: Set the correct names of the ports for auto detection on Windows platform here
+            // and uncomment this when port names are correct.
+            // list.add(new String[]{"Input Port 0"}, new String[]{"Output Port 0"});
+        } else if (platformType == PlatformType.LINUX) {
+            // TODO: Set the correct names of the ports for auto detection on Windows platform here
+            // and uncomment this when port names are correct.
+            // list.add(new String[]{"Input Port 0"}, new String[]{"Output Port 0"});
+        }
+    }
+
+    @Override
+    public MPD218Extension createInstance(final ControllerHost host) {
+        return new MPD218Extension(this, host);
+    }
 }
